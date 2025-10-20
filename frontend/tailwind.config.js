677d49ece764d3eada4e91614901b65779f98ce2
surlit <surlit29@gmail.com>
2025-10-20 12:18:28 -0400

    [UPD] se dejo de seguir correcion_frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Asegúrate de que las rutas sean correctas
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
