5bf778cc6ef99edbd8412caab7d1c4eeaf6d1cd5
surlit <surlit29@gmail.com>
2025-10-20 17:35:38 -0400

    [UPD] Se elimina a nivel visual y se actualiza la tarea si esta hecha o no

--- frontend/src/components/TasksCard.jsx
+++ frontend/src/components/TasksCard.jsx
@@ -10,36 +10,45 @@ export default function TasksCard({
   useEffect(()=>{
     if(data){
       console.log('data');
-      console.log(data);
+      // console.log(data);
     }
   })
   async function deleteTaskP (e){  
       
     
     if(data?.id){
-        console.log("en delete");      
-        console.log(data.id);
+        let tasks = tasksState.filter((task)=>task.id!=data.id)
         await deleteTask(data.id)
-        setTasks()
+        setTasks([...tasks])
       }
 
   }
   async function doneTaskP(e) {
       
     if (data?.id){
-        console.log("en done");            
-        console.log(data.id);
+        let tasks = tasksState.map((task)=>{
+          if(task.id == data.id){
+            task.done = !task.done
+          }
+          return task
+        })
+        setTasks([...tasks])
         await updateTask(data.id,{title:data.title,description:data.description,done:data.done})
 
       }
   }
   return (
-    <div key={key} id={data.id}className='mr-5 border-white border rounded-lg  text-xl text-center mb-1' >
-        <h3>{data.title} </h3>
-        <p>{data.description}</p>
-        <p>{data.done ? 'hecho ' : 'No hecho '}</p>
-        <p><button  onClick={doneTaskP}>{data.done ? '❌' : '✅'}</button></p>
-        <p><button onClick={deleteTaskP}>🗑️</button></p>
+    <div key={key} id={data.id} className='mr-5 border-white border rounded-lg  text-xl text-center mb-1 p-5 ' >
+        <span className='flex justify-between '>
+          <button title='Done task or not' onClick={doneTaskP}>{data.done ? '✅' : '❎'}</button> 
+          <button title='Delete Task' onClick={deleteTaskP}>🗑️</button>
+        </span>
+      
+        <h2 className='text-2xl '>{data.title}</h2>
+      
+        <p className=''>{data.description}</p>
+        <p className=''>{data.done ? 'hecho ' : 'No hecho '}</p>
+        
     </div>
   )
 }
